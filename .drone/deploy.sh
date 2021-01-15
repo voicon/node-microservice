0eabe98b968911e1b7ee0ed0fe4c00886bf23daf
Trần Nguyễn Xuân Vinh <vinhtnx@younetgroup.com>
2021-01-15 13:39:57 +0700

    Test

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#docker rm -f voicon_node_micro
-#docker rmi voicon/node-microservice
+docker rm -f voicon_node_micro
+docker rmi nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 docker run --rm -d -p 7777:3000 --name voicon_node_micro nazmulb/node-microservice
